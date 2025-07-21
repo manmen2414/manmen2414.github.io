bc7b775715519f25bf67b103e7dc5b38d66287ab
manmen2414 <inotitokokoro@gmail.com>
2025-07-21 20:07:04 +0900

    めんどくせぇ！Jqueryのほうが楽なんだ！！

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-import { createRoot } from "react-dom/client";
-
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
-
-// Render your React component instead
-const root = createRoot(document.getElementById("app"));
-root.render(<h1>Hello, world</h1>);
+$(() => {
+  $("h1").text("Powah");
+});
